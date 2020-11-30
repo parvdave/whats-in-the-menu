5cffcffb1c4796a2e008f8d80260955c86076048
Parv Dave <parvdave29@gmail.com>
2020-12-01 00:18:43 +0530

    Added django-heroku

--- whatsinthemenu/settings.py
+++ whatsinthemenu/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django-heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +28,7 @@ SECRET_KEY = 's+m*gq5$z5qw@tr@!4-qq)0#_hhlsn6fufhq&7$26jx%n&f^6h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['whatsinthemenu.herokuapp.com']
 
 # Application definition
 
@@ -129,3 +131,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+django-heroku.settings(locals())
